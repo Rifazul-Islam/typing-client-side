f360f8cb61ea77cf06a4c3feb001322d31e54a57
Rifazul <rifazul50@gmail.com>
2023-03-17 15:11:00 +0600

    add card section

--- src/component/Section1.tsx
+++ src/component/Section1.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import { BiBlanket} from "react-icons/bi";
+import { TfiEmail } from "react-icons/tfi";
+import { HiOutlineCalendarDays } from "react-icons/hi2";
 const Section1 = () => {
     return (
         <div className=' bg-slate-600 mt-3 mb-2 pb-5 mx-4'>
-               <div>
+        <div>
             <h1 className='text-center text-5xl text-white pt-5'> Universe of great insights</h1>
             <p className='text-center text-2xl text-white pt-5'>Typing skills? Check. Take the next step and make your online communication outstanding. </p>
                 
@@ -11,31 +13,34 @@ const Section1 = () => {
 
 <div className="card  bg-neutral text-neutral-content">
   <div className="card-body items-center text-center">
-    <h2 className="card-title">Cookies!</h2>
-    <p>We are using cookies for no reason.</p>
+    <h2 className=" text-white  text-5xl"> <BiBlanket/> </h2>
+    <h2 className="card-title text-3xl">Courses !</h2>
+    <p>in-depth knowledge thats easy to apply.</p>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Accept</button>
-      <button className="btn btn-ghost">Deny</button>
+      <button className="text-indigo-500 text-2xl">check courses</button>
+     
     </div>
   </div>
 </div>
 <div className="card  bg-neutral text-neutral-content">
   <div className="card-body items-center text-center">
-    <h2 className="card-title">Cookies!</h2>
-    <p>We are using cookies for no reason.</p>
+  <h2 className=" text-white  text-5xl"> <HiOutlineCalendarDays/> </h2>
+    <h2 className="card-title text-3xl"> Guides !</h2>
+    <p> Quick study with compprehensive on-pagers .</p>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Accept</button>
-      <button className="btn btn-ghost">Deny</button>
+      <button className="text-indigo-500 text-2xl"> Browse guides</button>
+     
     </div>
   </div>
 </div>
 <div className="card  bg-neutral text-neutral-content">
   <div className="card-body items-center text-center">
-    <h2 className="card-title">Cookies!</h2>
-    <p>We are using cookies for no reason.</p>
+  <h2 className=" text-line-white  text-5xl "> <TfiEmail/> </h2>
+    <h2 className="card-title text-3xl "> Email Templates !</h2>
+    <p> Task-oriented cheat sheets.</p>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Accept</button>
-      <button className="btn btn-ghost">Deny</button>
+      <button className="text-indigo-500 text-2xl">See templates </button>
+     
     </div>
   </div>
 </div>
